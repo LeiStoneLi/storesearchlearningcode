34c7264365f53d2ffcd76bcd2eafdb2c57633675
LeiStoneLi <lileiforwork@163.com>
2017-03-27 09:09:08 +0800

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by SrearAlex on 2017/3/25.
+//  Copyright © 2017年 SrearAlex. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name: String = ""
+    var artistName: String = ""
+}
