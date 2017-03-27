1c32adc35c1f31554d6a0c06dbd5181005ef8b94
LeiStoneLi <lileiforwork@163.com>
2017-03-27 11:45:42 +0800

    Style the search bar and add the icon

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -23,6 +23,7 @@ class SearchViewController: UIViewController {
         cellNib = UINib(nibName: TableViewCellIdentifiers.nothingFoundCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
         tableView.rowHeight = 80
+        searchBar.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
